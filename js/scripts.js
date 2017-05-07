3c20fc8114d7cbd9ed36cc8d3e4d1cf8952d10eb
Håkon Underbakke <hakonunderbakke@Hakons-MacBook-Pro.local>
2017-05-07 15:45:33 +0200

    a penis is literally a fountain of youth

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,9 @@ $(".show-createSite, .cancel-createSite").click(function(){
 	$(".show-createSite").toggle();
 });
 function createSite(){
-	$(".main").append("<div class='section siteCreation'></div>").load("templates/siteCreation.html");
+	$(".main").append("<div class='section sitecreation'><header class='tools'><i class='fa fa-window-close-o'></i> " + $("input[name='siteName']").val() + "</header></div>");
+	$(".sitecreation").load("templates/siteCreation.html");
 }
+$("body").on("click", "header.tools .fa-window-close-o", function(){
+	$(this).closest(".section").remove();
+});
