bddff9ec1f1a0e9d05be965de676320c669d9101
Håkon Underbakke <hakonunderbakke@Hakons-MacBook-Pro.local>
2017-05-07 16:04:22 +0200

    cheap sheep

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,9 @@ $('a[href="#"]').click(function(e){
 $(".show-createSite, .cancel-createSite").click(function(){
 	$(".createSite").toggle();
 	$(".show-createSite").toggle();
+	if ($(".show-createSite:visible")){
+		$("input[name='siteName'").focus();
+	}
 });
 function createSite(){
 	$(".main").append("<div class='section sitecreation'><header class='tools'><i class='fa fa-window-close-o'></i> " + $("input[name='siteName']").val() + "</header></div>");
@@ -12,4 +15,18 @@ function createSite(){
 }
 $("body").on("click", "header.tools .fa-window-close-o", function(){
 	$(this).closest(".section").remove();
+});
+$(".ac-account, .ac-guest").click(function(){
+	if ($(".ac-guest").prop("checked") === true){
+		$(".section.account").hide();
+	}else{
+		$(".section.account").show();
+	}
+});
+$(document).ready(function(){
+	if ($(".ac-guest").prop("checked") === true){
+		$(".section.account").hide();
+	}else{
+		$(".section.account").show();
+	}
 });
