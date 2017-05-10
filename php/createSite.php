545e82365521e9742de6b2781235a9aa99cc64db
Morten Hauge <Morten.hauge97@gmail.com>
2017-05-10 23:45:22 +0200

    Modifid createSite.php

--- php/createSite.php
+++ php/createSite.php
@@ -1,5 +1,6 @@
 <?php
 
+// Initialize variables
 $name;
 $theme;
 $jQuery;
@@ -8,6 +9,7 @@ $meta_name;
 $meta_keywords;
 $meta_description;
 
+// Check if includejQuery is checked
 function includejQuery() {
   if(isset($_POST['includejQuery'])) {
     $jquery = '<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>';
@@ -16,6 +18,7 @@ function includejQuery() {
   }
 }
 
+// Check if normalizeCSS is checked
 function includeNormalize() {
   if(isset($_POST['normalizeCSS'])) {
     $normalize = '<link rel="stylesheet" href="css/normalize.css">';
@@ -26,11 +29,13 @@ function includeNormalize() {
 
 // Assigns the basic info to the proper variables
 function setBasicInfo() {
-  if(isset($_POST['name'])) {
-    $name = $_POST['name']; // Get the site name
+  if(isset($_POST['siteName'])) {
+    $name = $_POST['siteName']; // Get the site name
+  } else {
+    $name = "hello-world";
   }
-  if(isset($_POST['name'])) {
-    $theme = $_POST['theme']; // Grabs the selected theme
+  if(isset($_POST['siteTheme'])) {
+    $theme = $_POST['siteTheme']; // Grabs the selected theme
   }
   includejQuery();
   includeNormalize();
@@ -49,33 +54,16 @@ function setMetaData() {
   }
 }
 
+// Run the necessary functions
 setBasicInfo();
 setMetaData();
+
 ?>
 
-<script type="text/javascript">
-var name = "<?php echo($name); ?>";
-var theme = "<?php echo($theme); ?>";
-var jQuery = "<?php echo($jQuery); ?>";
-var normalize = "<?php echo($normalize); ?>";
-var meta_name = "<?php echo($meta_name); ?>";
-var meta_keywords = "<?php echo($meta_keywords); ?>";
-var meta_description = "<?php echo($meta_description); ?>";
-
-var str = `
 <html>
-  <head>
-    <meta name="author" content="${meta_name}">
-    <meta name="keywords" content="${meta_keywords}">
-    <meta name="description" content="${meta_description}">
-    ${normalize}
-    <link rel="stylesheet" href="css/main.css">
-    <title>${name}</title>
-    <body>
-      <script src="${jQuery}"></script>
-    </body>
-    ${jQuery}
-  </head>
+<head>
+</head>
+<body>
+  <p><?php echo($name); ?></p>
+</body>
 </html>
-`;
-</script>
