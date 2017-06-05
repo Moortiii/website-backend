112e5f3a47b1d6b4e052b4f4caba7672617db45f
Morten Hauge <Morten.hauge97@gmail.com>
2017-06-05 22:01:08 +0200

    It's now possible to include Flexgrid in the CSS Folder in the ZIP

--- php/createSite.php
+++ php/createSite.php
@@ -14,12 +14,6 @@
   $meta_author;
   $titleformat;
 
-  /* Button Variables */
-
-  $flat;
-  $rounded;
-  $btn3d;
-
   function includejQuery() {
     global $jQuery;
     if(isset($_POST['includejQuery'])) {
@@ -117,7 +111,6 @@
 <script type="text/javascript">
 
     // Function definitions
-
     function normalize_css_styles() {
       var normalizeCSS;
       $.ajax({
@@ -128,6 +121,18 @@
         }
       });
       return normalizeCSS;
+    }
+
+    function includeFlexGrid() {
+      var flexGrid;
+      $.ajax({
+        url: '../templateCSS/flexgrid.css',
+        async: false,
+        success: function(response) {
+          flexGrid = response;
+        }
+      })
+      return flexGrid;
     }
 
     function btn_default_styles() {
@@ -179,7 +184,6 @@
     }
 
     var name = "<?php echo($name); ?>";
-    document.title = name;
     var titleformat = "<?php echo($titleformat); ?>";
     var theme = "<?php echo($theme); ?>";
     var jQuery = "<?php echo($jQuery); ?>";
@@ -187,9 +191,9 @@
     var meta_name = "<?php echo($meta_name); ?>";
     var meta_author = "<?php echo($meta_author); ?>";
     var page = "<?php echo ($page) ?>";
-    var grid = "";
     var filename = name.replace(/[^a-z0-9]/gi, '_').toLowerCase() + "_" + page.replace(/[^a-z0-9]/gi, '_').toLowerCase() + ".zip";
     var list = [];
+    document.title = name;
 
     <?php
       $sCounter = 0;
@@ -215,45 +219,50 @@
   </head>
   <body>
     <h1>${name}</h1>
-    ${grid}
     ${list}
     ${jQuery}
   </body>
 </html>`;
 
+      /* Create a new ZIP */
       var root = new JSZip();
       root.file("index.html", str);
 
       var normalize_css = normalize_css_styles();
       var button_default_css = btn_default_styles();
+
+      /* Include default button styles */
       var btn_complete = button_default_css;
 
+      /* Create the folder to hold the CSS */
       var css = root.folder("css");
       css.file("normalize.css", normalize_css);
 
+      /* Create the folder to hold the scripts */
       var scripts = root.folder("scripts");
-      scripts.file("scripts.js", "alert('Hello World')");
+
+      /* Include Flexgrid */
+      <?php if(isset($_POST['themeGrid'])) { ?>
+        var flexGrid = includeFlexGrid();
+        css.file("flexgrid.css", flexGrid);
+      <?php } ?>
 
       /* Include the button styles the user selected using checkboxes */
-      <?php if(isset($_POST['flat-check'])) { ?>
-              var btn_flat_css = btn_flat_styles();
-              btn_complete += btn_flat_css;
-              // css.file("btn-flat.css", btn_flat_css);
-            <?php }
-            if(isset($_POST['rounded-check'])) { ?>
-              var btn_rounded_css = btn_rounded_styles();
-              btn_complete += btn_rounded_css;
-              // css.file("btn-rounded.css", btn_rounded_css);
-            <?php }
-            if(isset($_POST['btn3d-check'])) { ?>
-              var btn_3d_css = btn_3d_styles();
-              btn_complete += btn_3d_css;
-              // css.file("btn-3d.css", btn_3d_css);
+      <?php if(isset($_POST['enableFormDesign'])) {
+              if(isset($_POST['flat-check'])) { ?>
+                var btn_flat_css = btn_flat_styles();
+                btn_complete += btn_flat_css;
+              <?php }
+              if(isset($_POST['rounded-check'])) { ?>
+                var btn_rounded_css = btn_rounded_styles();
+                btn_complete += btn_rounded_css;
+              <?php }
+              if(isset($_POST['btn3d-check'])) { ?>
+                var btn_3d_css = btn_3d_styles();
+                btn_complete += btn_3d_css;
+            <?php } ?>
+            css.file("btn-styles.css", btn_complete);
           <?php } ?>
-
-      /* We ahve to add the css file once all checks are completed */
-
-    css.file("btn-styles.css", btn_complete);
 
     function downloadBoilerplate() {
       root.generateAsync({
